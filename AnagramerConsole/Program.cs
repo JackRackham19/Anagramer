650503102c3bd883b992a083d1a5e49031b58752
Martin Strobel <robert.s.maltin@gmail.com>
2016-03-14 02:23:42 -0700

    Adding CommandLine Arguments.

--- AnagramerConsole/Program.cs
+++ AnagramerConsole/Program.cs
@@ -3,16 +3,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AnagramerConsole
 {
     class Program
     {
+        private static readonly Regex dictionaryArgPattern = new Regex("^[/-]d(?:ictionary)?$", RegexOptions.Compiled);
+        private static readonly Regex maxWordsArgPattern = new Regex("^[/-]w(?:ords)?$", RegexOptions.Compiled);
+        
         static void Main(string[] args)
         {
-            var dictionary = Trie.ReadTrieAsync(@"C:\Users\Martin\OneDrive\Documents\Programming\12dicts-5.0\3esl.txt", s => s.ToLowerInvariant()).Result;
-            var results = Anagram.Find(args[0], dictionary, 3);
+            string dictionaryPath = "dictionary.txt";
+            uint maxWords = 4;
+            
+            for(int i = 0; i < args.Length; i++)
+            {
+                if(dictionaryArgPattern.IsMatch(args[i]))
+                {
+                    dictionaryPath = args[++i];
+                }
+                if(maxWordsArgPattern.IsMatch(args[i]))
+                {
+                    maxWords = uint.Parse(args[++i]);
+                }
+            }
+
+            var dictionary = Trie.ReadTrieAsync(dictionaryPath, s => s.ToLowerInvariant()).Result;
+            var results = Anagram.Find(args[0], dictionary, maxWords);
             foreach(var entry in results)
             {
                 Console.WriteLine(entry);
